b0a56af5ad782ac24f0dcb15c46755655de7ca64
SyedEbad902 <ebad3088@gmail.com>
2024-08-01 06:16:25 +0500

    splash screen and home page added

---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:pdf_scanner/pages/home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+
+
+class _SplashScreenState extends State<SplashScreen> {
+void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 60), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => const HomeScreen()));
+    });
+  }  
+  @override
+  
+  Widget build(BuildContext context) {
+    return  SafeArea(
+      child: Scaffold(
+        body: Container(
+          // color: const Color.fromRGBO(83, 177, 117, 5),
+          child: Center(
+            child: SizedBox(
+              height: double.infinity,
+              width: double.infinity,
+              child:  SvgPicture.asset("assets/images/Splash Screen.svg"),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
